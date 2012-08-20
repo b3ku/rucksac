c9f767fa845d4629d2c95324c83d6fac2f27c7fd
Oliver Becker <ob@obqo.de>
2012-08-20 09:12:53 +0200

    removed final keyword

--- src/main/scala/org/rucksac/parser/Conditions.scala
+++ src/main/scala/org/rucksac/parser/Conditions.scala
@@ -18,7 +18,7 @@ final class CombinatorCondition(first: Condition, second: Condition) extends Con
 
 }
 
-final object conditions {
+object conditions {
 
     final class NegativeCondition(con: Condition) extends Condition {
 
